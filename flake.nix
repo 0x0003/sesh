741dd8987470bf09f5215dd2f0e24c6ae4398072
0x0003 <0x0003@users.noreply.github.com>
2024-07-16 10:10:56 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      version = "1.2.0";
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          go-hello = pkgs.buildGoModule {
+            pname = "sesh-test";
+            inherit version;
+            src = ./.;
+            vendorHash = "sha256-zt1/gE4bVj+3yr9n0kT2FMYMEmiooy3k1lQ77rN6sTk=";
+          };
+        });
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ go gopls gotools go-tools ];
+          };
+        });
+      defaultPackage = forAllSystems (system: self.packages.${system}.go-hello);
+    };
+}
